6561879392705db06a8ec9c6ff9af8810501821c
Rafael Romano Silva <romanosilvarafael@gmail.com>
2024-10-01 21:26:16 -0300

    criando a classe posicao

--- xadrez-console/tabuleiro/Program.cs
+++ xadrez-console/tabuleiro/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace tabuleiro
+﻿using xadrez_console.tabuleiro;
+
+namespace xadrez_console
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine($"Posição: {P}");
+            Console.ReadLine();
         }
     }
 }
