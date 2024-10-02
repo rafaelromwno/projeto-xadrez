9bee48c768da9b8874e8384113d76591ead5fe5a
Rafael Romano Silva <romanosilvarafael@gmail.com>
2024-10-01 21:35:45 -0300

    comecando a implementar o tabuleiro

--- xadrez-console/tabuleiro/tabuleiro/Posicao.cs
+++ xadrez-console/tabuleiro/tabuleiro/Posicao.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace xadrez_console.tabuleiro
 {
-    internal class Posicao
+    public class Posicao
     {
         public int Linha { get; set; }
         public int Coluna { get; set; }
