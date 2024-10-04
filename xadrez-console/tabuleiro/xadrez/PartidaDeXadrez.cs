ac9c702fe51b67e4f5822295b182488f1e96ceb1
Rafael Romano Silva <romanosilvarafael@gmail.com>
2024-10-04 20:58:43 -0300

    jogada especial: promocao

--- xadrez-console/tabuleiro/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/tabuleiro/xadrez/PartidaDeXadrez.cs
@@ -145,6 +145,22 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = Tab.GetPeca(destino);
+
+            // #jogadaespecial - promocao
+            if(p is Peao)
+            {
+                if((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    Pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    Pecas.Add(dama);
+                }
+            }
+
+
             if (EstaEmXeque(Adversaria(JogadorAtual)))
             {
                 Xeque = true;
@@ -163,8 +179,6 @@ namespace xadrez
                 Turno++;
                 MudaJogador();
             }
-
-            Peca p = Tab.GetPeca(destino);
 
             // #jogadaespecial - En Passant
             if (p is Peao && (destino.Linha == origem.Linha - 2 || (destino.Linha == origem.Linha + 2)))
